caa39dc2fe7f87b780baf90f8b9f65c762f8dc2a
Jack Kelly <jack-list@xlk.org.uk>
2015-10-21 11:35:36 +0100

    Structure of talk complete

--- js/reveal-init.js
+++ js/reveal-init.js
@@ -8,6 +8,19 @@ Reveal.initialize({
 
     transition: 'slide', // none/fade/slide/convex/concave/zoom
 
+    // The "normal" size of the presentation, aspect ratio will be preserved
+    // when the presentation is scaled to fit different resolutions. Can be
+    // specified using percentage units.
+    width: 1000,
+    height: 750,
+
+    // Factor of the display size that should remain empty around the content
+    margin: 0.05,
+
+    // Bounds for smallest/largest possible scale to apply to content
+    minScale: 0.2,
+    maxScale: 1.5,
+
     // Optional reveal.js plugins
     dependencies: [
         { src: 'bower_components/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
